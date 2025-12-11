16278b3237ed5cb27b6b1bafd1c5a145076d4956
qaiik <qaiikprogramming@gmail.com>
2025-12-11 13:51:02 -0700

    .

--- opcodes/opcodes.go
+++ opcodes/opcodes.go
@@ -3,7 +3,6 @@ package opcodes
 import "fmt"
 
 func Lookup_Op(op string) (byte, error) {
-	//a switch is benchmarkedly fastest
 	switch op {
 	case "i32.const":
 		return 0x41, nil
@@ -13,12 +12,19 @@ func Lookup_Op(op string) (byte, error) {
 		return 0x43, nil
 	case "f64.const":
 		return 0x44, nil
+	case "i32.add":
+		return 0x6A, nil
+	case "i64.add":
+		return 0x7C, nil
+	case "f32.add":
+		return 0x92, nil
+	case "f64.add":
+		return 0xA0, nil
 	}
 	return 0, fmt.Errorf("unknown operation %s", op)
 }
 
 func Lookup_Op_Back(op byte) (string, error) {
-	// switch is still the fastest
 	switch op {
 	case 0x41:
 		return "i32.const", nil
@@ -28,6 +34,14 @@ func Lookup_Op_Back(op byte) (string, error) {
 		return "f32.const", nil
 	case 0x44:
 		return "f64.const", nil
+	case 0x6A:
+		return "i32.add", nil
+	case 0x7C:
+		return "i64.add", nil
+	case 0x92:
+		return "f32.add", nil
+	case 0xA0:
+		return "f64.add", nil
 	}
 	return "", fmt.Errorf("unknown opcode 0x%X", op)
 }
